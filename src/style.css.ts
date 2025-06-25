edf81ae88f641a71b4425aacdd77a0ce071dfa27
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-06-25 15:05:34 +0200

    fix

--- src/style.css.ts
+++ src/style.css.ts
@@ -1,4 +1,5 @@
-import { globalStyle, style } from '@vanilla-extract/css';
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 const bottomBtn = style({
   position: 'fixed',
@@ -6,6 +7,9 @@ const bottomBtn = style({
   width: '100%',
   padding: '12px',
   bottom: 0,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '.25rem',
 });
 
 const container = style({
@@ -15,13 +19,15 @@ const container = style({
   gap: '1rem',
 });
 
-const box = style({
+const boxCenter = style({
   display: 'flex',
-  padding: '1rem',
+  padding: '2rem 1rem 1rem',
   flexDirection: 'column',
-  gap: '1rem',
-  borderRadius: '1rem',
+  gap: '.5rem',
   backgroundColor: '#F3F4F5',
+  justifyContent: 'center',
+  textAlign: 'center',
+  alignItems: 'center',
 });
 
 const row = style({
@@ -29,17 +35,59 @@ const row = style({
   alignItems: 'center',
   justifyContent: 'space-between',
 });
+const rowCenter = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '4px',
+  color: '#2A77EF',
+});
 
-const switchItem = style({});
+const sliderCard = recipe({
+  base: {
+    position: 'relative',
+    backgroundColor: '#F3F4F5',
+    padding: '1rem',
+    borderRadius: '1rem',
+    height: '96px',
+    width: 'max-content',
+    transition: 'background-color 0.25s ease-in-out',
+  },
+  variants: {
+    selected: {
+      true: {
+        backgroundColor: 'black',
+      },
+    },
+  },
+});
 
-globalStyle(`${switchItem} > span > span:first-child`, {
-  fontWeight: 500,
+const sliderCardIcon = style({
+  position: 'absolute',
+  top: -5,
+  right: -5,
+  borderRadius: '1rem',
+  backgroundColor: 'white',
+  width: 'max-content',
+  display: 'flex',
+  padding: '4px',
+});
+
+const box = style({
+  backgroundColor: '#F3F4F5',
+  padding: '1rem',
+  borderRadius: '1rem',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '.5rem',
 });
 
 export const appSt = {
   bottomBtn,
   container,
-  box,
+  boxCenter,
   row,
-  switchItem,
+  sliderCard,
+  sliderCardIcon,
+  rowCenter,
+  box,
 };
